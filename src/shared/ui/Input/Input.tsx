567a9a935fbd3d6a943f15729771f4ad26ac402c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-11 21:46:10 +0200

    chore: create Input component

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Input: FC = () => {
+  return (
+    <div className="">
+      <label htmlFor=""></label>
+      <input id="" type="" />
+    </div>
+  );
+};
+
+export default Input;
