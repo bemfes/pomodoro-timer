ff5a351a3f1544534fcd772a54087e196fc8cf34
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-12 21:21:33 +0200

    test: write tests for Modal component

---
+++ src/shared/ui/Modal/Modal.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Modal from "./Modal";
+
+describe("Modal", () => {
+  it("should render children", () => {
+    render(
+      <Modal isOpen={true} onClick={jest.fn()}>
+        <p>modal is open</p>
+      </Modal>,
+    );
+    expect(screen.getByText(/modal is open/i)).toBeInTheDocument();
+  });
+  it("should render close modal button", () => {
+    render(
+      <Modal isOpen={true} onClick={jest.fn()}>
+        <p>modal is open</p>
+      </Modal>,
+    );
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+  it("should not render modal when isOpen is false", () => {
+    render(
+      <Modal isOpen={false} onClick={jest.fn()}>
+        <p>modal is open</p>
+      </Modal>,
+    );
+    expect(screen.queryByText(/modal is open/i)).not.toBeInTheDocument();
+  });
+  it("should invoke onClick callback when user clicks on close modal button", () => {
+    const onClick = jest.fn();
+    render(
+      <Modal isOpen={true} onClick={onClick}>
+        <p>modal is open</p>
+      </Modal>,
+    );
+    const elem = screen.getByRole("button");
+    fireEvent.click(elem);
+    expect(onClick).toHaveBeenCalled();
+  });
+});
