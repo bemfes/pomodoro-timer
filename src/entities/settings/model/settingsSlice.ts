3215ee146bbc0cfb085e1b7f26003dc5ba015157
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-13 19:26:25 +0200

    chore: create initial state

--- src/entities/settings/model/settingsSlice.ts
+++ src/entities/settings/model/settingsSlice.ts
@@ -1,6 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+type Colors = "red" | "blue" | "purple";
+
+type Fonts = "manrope" | "georgian" | "jet";
+
+interface InitialState {
+  pomodoro: number;
+  shortBreak: number;
+  longBreak: number;
+  color: Colors;
+  font: Fonts;
+}
+
+const initialState: InitialState = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  color: "red",
+  font: "manrope",
+};
 
 const settingsSlice = createSlice({
   name: "settings",
