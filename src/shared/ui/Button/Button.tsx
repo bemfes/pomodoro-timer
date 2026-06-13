134657a3b6f9f8cead32b4ac766cdd1c7437fd83
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-13 12:17:34 +0200

    style: create basic styles for Button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,17 +1,18 @@
-import React, { FC } from "react"
+import React, { FC } from "react";
+import styles from "./Button.module.css";
 
 interface ButtonProps {
-    children: React.ReactNode;
-    className: string;
-    onClick: () => void;
+  children: React.ReactNode;
+  className: string;
+  onClick: () => void;
 }
 
 const Button: FC<ButtonProps> = ({ children, className, onClick }) => {
-    return (
-        <button className={className} onClick={onClick}>
-            {children}
-        </button>
-    )
-}
+  return (
+    <button className={`${styles.btn} ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
