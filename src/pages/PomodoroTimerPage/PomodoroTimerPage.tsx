95c5b280853a4a38c366acab2ff9165bc6844a76
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-13 11:39:07 +0200

    feat: create page for Pomodoro Timer

---
+++ src/pages/PomodoroTimerPage/PomodoroTimerPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import styles from "./PomodoroTimerPage.module.css";
+
+const PomodoroTimerPage: FC = () => {
+  return (
+    <>
+      <p className={styles.title}>pomodoro</p>
+    </>
+  );
+};
+
+export default PomodoroTimerPage;
