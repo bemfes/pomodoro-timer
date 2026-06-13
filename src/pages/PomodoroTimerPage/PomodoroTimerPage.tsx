12cb436e7b38930fa69e6c1dc85e41ab9647e4c2
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-13 12:15:29 +0200

    feat: add settings widget

--- src/pages/PomodoroTimerPage/PomodoroTimerPage.tsx
+++ src/pages/PomodoroTimerPage/PomodoroTimerPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import styles from "./PomodoroTimerPage.module.css";
+import SettingsModal from "@/widgets/SettingsModal/ui";
 
 const PomodoroTimerPage: FC = () => {
   return (
     <>
       <p className={styles.title}>pomodoro</p>
+      <SettingsModal />
     </>
   );
 };
