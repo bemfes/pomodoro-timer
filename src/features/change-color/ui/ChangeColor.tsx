2c7034a4b825d4dbc2461e2b05aaf3a966e20339
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-14 12:55:58 +0200

    fix: add key prop

--- src/features/change-color/ui/ChangeColor.tsx
+++ src/features/change-color/ui/ChangeColor.tsx
@@ -14,6 +14,7 @@ const ChangeColor: FC = () => {
     <div className={styles.container}>
       {colors.map((colorItem: Colors) => (
         <Button
+          key={colorItem}
           className={`${styles.btn} ${styles[colorItem]}`}
           onClick={() => handleChangeColor(colorItem)}
         >
