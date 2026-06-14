3d1beabdda64b099abe92ec0b02379c22ddd8862
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-14 12:39:15 +0200

    refactor: render colors buttons with map

--- src/features/change-color/ui/ChangeColor.tsx
+++ src/features/change-color/ui/ChangeColor.tsx
@@ -2,19 +2,21 @@ import { FC } from "react";
 import IconCheck from "@/shared/assets/images/icon-check.svg";
 import Button from "@/shared/ui/Button";
 import styles from "./ChangeColor.module.css";
+import { Colors } from "@/entities/settings";
 
 const ChangeColor: FC = () => {
+  const colors: Colors[] = ["red", "blue", "purple"];
+
   return (
     <div className={styles.container}>
-      <Button className={`${styles.btn} ${styles.red}`} onClick={() => {}}>
-        <IconCheck />
-      </Button>
-      <Button className={`${styles.btn} ${styles.blue}`} onClick={() => {}}>
-        <IconCheck />
-      </Button>
-      <Button className={`${styles.btn} ${styles.purple}`} onClick={() => {}}>
-        <IconCheck />
-      </Button>
+      {colors.map((colorItem: Colors) => (
+        <Button
+          className={`${styles.btn} ${styles[colorItem]}`}
+          onClick={() => {}}
+        >
+          <IconCheck />
+        </Button>
+      ))}
     </div>
   );
 };
