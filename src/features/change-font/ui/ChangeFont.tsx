4424655369cfb4fa937cf0d2386d98484cc609d2
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-14 12:31:59 +0200

    refactor: render font buttons with map

--- src/features/change-font/ui/ChangeFont.tsx
+++ src/features/change-font/ui/ChangeFont.tsx
@@ -1,22 +1,22 @@
 import Button from "@/shared/ui/Button";
 import { FC } from "react";
 import styles from "./ChangeFont.module.css";
+import type { Fonts } from "@/entities/settings";
 
 const ChangeFont: FC = () => {
+  const fonts: Fonts[] = ["manrope", "georgian", "jet"];
+
   return (
     <div className={styles.container}>
-      <Button
-        className={`${styles.btn} ${styles.manrope} ${styles.active}`}
-        onClick={() => {}}
-      >
-        Aa
-      </Button>
-      <Button className={`${styles.btn} ${styles.georgian}`} onClick={() => {}}>
-        Aa
-      </Button>
-      <Button className={`${styles.btn} ${styles.jet}`} onClick={() => {}}>
-        Aa
-      </Button>
+      {fonts.map((fontItem: Fonts) => (
+        <Button
+          key={fontItem}
+          className={`${styles.btn} ${styles[fontItem]} ${styles.active}`}
+          onClick={() => {}}
+        >
+          Aa
+        </Button>
+      ))}
     </div>
   );
 };
