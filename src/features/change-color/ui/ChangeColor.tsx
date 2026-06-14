8bf73fd5fa00ca2c65f4f640a786cd27d98827a5
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-14 12:55:00 +0200

    feat: integrate custom hook useChangeColor

--- src/features/change-color/ui/ChangeColor.tsx
+++ src/features/change-color/ui/ChangeColor.tsx
@@ -3,18 +3,21 @@ import IconCheck from "@/shared/assets/images/icon-check.svg";
 import Button from "@/shared/ui/Button";
 import styles from "./ChangeColor.module.css";
 import { Colors } from "@/entities/settings";
+import { useChangeColor } from "../model/useChangeColor";
 
 const ChangeColor: FC = () => {
   const colors: Colors[] = ["red", "blue", "purple"];
+
+  const { color, handleChangeColor } = useChangeColor();
 
   return (
     <div className={styles.container}>
       {colors.map((colorItem: Colors) => (
         <Button
           className={`${styles.btn} ${styles[colorItem]}`}
-          onClick={() => {}}
+          onClick={() => handleChangeColor(colorItem)}
         >
-          <IconCheck />
+          {color === colorItem && <IconCheck />}
         </Button>
       ))}
     </div>
