68666740d135484d8862ccc10eba493e55df852a
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-14 12:33:47 +0200

    feat: integrate custom hook useChangeFont

--- src/features/change-font/ui/ChangeFont.tsx
+++ src/features/change-font/ui/ChangeFont.tsx
@@ -2,17 +2,20 @@ import Button from "@/shared/ui/Button";
 import { FC } from "react";
 import styles from "./ChangeFont.module.css";
 import type { Fonts } from "@/entities/settings";
+import { useChangeFont } from "../model/useChangeFont";
 
 const ChangeFont: FC = () => {
   const fonts: Fonts[] = ["manrope", "georgian", "jet"];
+
+  const { font, handleChangeFont } = useChangeFont();
 
   return (
     <div className={styles.container}>
       {fonts.map((fontItem: Fonts) => (
         <Button
           key={fontItem}
-          className={`${styles.btn} ${styles[fontItem]} ${styles.active}`}
-          onClick={() => {}}
+          className={`${styles.btn} ${styles[fontItem]} ${font === fontItem ? styles.active : ""}`}
+          onClick={() => handleChangeFont(fontItem)}
         >
           Aa
         </Button>
