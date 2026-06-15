0c8ecd82cdef782005cccb43924a79e83a6fa137
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-15 12:48:17 +0200

    feat: add Pomodoro Timer

--- src/pages/PomodoroTimerPage/PomodoroTimerPage.tsx
+++ src/pages/PomodoroTimerPage/PomodoroTimerPage.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import styles from "./PomodoroTimerPage.module.css";
 import SettingsModal from "@/widgets/SettingsModal";
+import PomodoroTimer from "@/widgets/PomodoroTimer";
 
 const PomodoroTimerPage: FC = () => {
   return (
     <>
       <p className={styles.title}>pomodoro</p>
+      <PomodoroTimer />
       <SettingsModal />
     </>
   );
