465b53c214ed9b032b0517293567bc76ab874a31
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-16 13:31:02 +0200

    feat: add styles to pomodoro timer container

--- src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
+++ src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
@@ -1,12 +1,13 @@
 import ChooseTimerMode from "@/features/choose-timer-mode";
 import Button from "@/shared/ui/Button";
 import { usePomodoroTimer } from "../model/usePomodoroTimer";
+import styles from "./PomodoroTimer.module.css";
 
 const PomodoroTimer = () => {
   const { isRunning, timeLeft, handleChangeIsRunning } = usePomodoroTimer();
 
   return (
-    <div>
+    <div className={styles.container}>
       <ChooseTimerMode />
       <p>timer: {timeLeft}</p>
       <Button className="" onClick={handleChangeIsRunning}>
