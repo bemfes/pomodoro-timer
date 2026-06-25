1f4f3f24f7196bff5226f6fb4f84931726dc9891
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-25 12:32:56 +0200

    feat: create circular progress bar

---
+++ src/shared/ui/CircleProgressBar/CircleProgressBar.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from "react";
+import styles from "./CircleProgressBar.module.css";
+
+interface CircleProgressBarProps {
+  percentage: number;
+  strokeColorActive: string;
+  strokeColorBg: string;
+  strokeWidth: number;
+  children: React.ReactNode;
+  radius: number;
+}
+
+const CircleProgressBar: FC<CircleProgressBarProps> = ({
+  percentage,
+  strokeColorActive,
+  strokeColorBg,
+  strokeWidth,
+  children,
+  radius,
+}) => {
+  const circleRadius = radius - strokeWidth / 2;
+  const viewBox = `0 0 ${radius * 2} ${radius * 2}`;
+
+  const circleLength = 2 * Math.PI * circleRadius;
+
+  const strokeDasharray = circleLength;
+  const strokeDashoffset = circleLength * (1 - percentage / 100);
+  return (
+    <div
+      style={{ width: radius * 2, height: radius * 2 }}
+      className={styles.progress}
+    >
+      <svg viewBox={viewBox} className={styles.svg}>
+        <circle
+          fill="none"
+          strokeWidth={strokeWidth}
+          stroke={strokeColorBg}
+          cx={radius}
+          cy={radius}
+          r={circleRadius}
+        />
+        <circle
+          strokeDashoffset={strokeDashoffset}
+          strokeDasharray={strokeDasharray}
+          strokeLinecap="round"
+          fill="none"
+          strokeWidth={strokeWidth}
+          stroke={strokeColorActive}
+          className={styles.active}
+          cx={radius}
+          cy={radius}
+          r={circleRadius}
+        />
+      </svg>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+};
+
+export default CircleProgressBar;
