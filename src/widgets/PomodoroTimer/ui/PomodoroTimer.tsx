4d18b89becff2fa935fc7fc5c360157d10d5e475
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-26 21:51:26 +0200

    chore: add wrapper for circle progress bar

--- src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
+++ src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
@@ -11,20 +11,22 @@ const PomodoroTimer = () => {
   return (
     <div className={styles.container}>
       <ChooseTimerMode />
-      <CircleProgressBar
-        percentage={percentage}
-        strokeColorActive={`var(--${color})`}
-        strokeColorBg="transparent"
-        strokeWidth={12}
-        radius={180}
-      >
-        <div className={styles.box}>
-          <p className={styles.time}>{timeLeft}</p>
-          <Button className={styles.btn} onClick={handleChangeIsRunning}>
-            {isRunning ? "PAUSE" : "RUN"}
-          </Button>
-        </div>
-      </CircleProgressBar>
+      <div className={styles.ring}>
+        <CircleProgressBar
+          percentage={percentage}
+          strokeColorActive={`var(--${color})`}
+          strokeColorBg="transparent"
+          strokeWidth={12}
+          radius={180}
+        >
+          <div className={styles.box}>
+            <p className={styles.time}>{timeLeft}</p>
+            <Button className={styles.btn} onClick={handleChangeIsRunning}>
+              {isRunning ? "PAUSE" : "RUN"}
+            </Button>
+          </div>
+        </CircleProgressBar>
+      </div>
     </div>
   );
 };
