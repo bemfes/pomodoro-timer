02b5272518c93687a77b268a277dd7e13c9e9112
Anastasiia Ustin <2anastasiia@mail.ru>
2026-06-28 10:50:44 +0200

    fix: change timeLeft to minutes and seconds

--- src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
+++ src/widgets/PomodoroTimer/ui/PomodoroTimer.tsx
@@ -5,8 +5,14 @@ import styles from "./PomodoroTimer.module.css";
 import CircleProgressBar from "@/shared/ui/CircleProgressBar";
 
 const PomodoroTimer = () => {
-  const { isRunning, timeLeft, color, percentage, handleChangeIsRunning } =
-    usePomodoroTimer();
+  const {
+    isRunning,
+    minutes,
+    seconds,
+    color,
+    percentage,
+    handleChangeIsRunning,
+  } = usePomodoroTimer();
 
   return (
     <div className={styles.container}>
@@ -21,7 +27,9 @@ const PomodoroTimer = () => {
             radius={180}
           >
             <div className={styles.box}>
-              <p className={styles.time}>{timeLeft}</p>
+              <p className={styles.time}>
+                {minutes}:{seconds}
+              </p>
               <Button className={styles.btn} onClick={handleChangeIsRunning}>
                 {isRunning ? "PAUSE" : "RUN"}
               </Button>
