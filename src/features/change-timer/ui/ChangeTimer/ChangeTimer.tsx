8dd83c471eae257ea819e0757b1ea60f9d467009
Anastasiia Ustin <2anastasiia@mail.ru>
2026-07-01 10:45:25 +0200

    refactor: change Input component to TimerInput component

--- src/features/change-timer/ui/ChangeTimer/ChangeTimer.tsx
+++ src/features/change-timer/ui/ChangeTimer/ChangeTimer.tsx
@@ -1,41 +1,42 @@
-import Input from "@/shared/ui/Input";
 import styles from "./ChangeTimer.module.css";
 import { useChangeTimer } from "../../model/useChangeTimer";
+import TimerInput from "../TimerInput/TimerInput";
 
 const ChangeTimer = () => {
   const { pomodoroInput, shortBreakInput, longBreakInput } = useChangeTimer();
 
   return (
     <div className={styles.grid}>
-      <Input
-        className={styles.container}
-        type="number"
+      <TimerInput
         value={pomodoroInput.value}
         htmlFor="pomodoro"
         id="pomodoro"
         labelText="pomodoro"
         onChange={(e) => pomodoroInput.handleInputChange(e)}
         onBlur={pomodoroInput.handleInputBlur}
+        handleIncrease={pomodoroInput.handleIncrease}
+        handleDecrease={pomodoroInput.handleDecrease}
       />
-      <Input
-        className={styles.container}
-        type="number"
+      <TimerInput
         value={shortBreakInput.value}
         htmlFor="short-break"
         id="short-break"
         labelText="short break"
         onChange={(e) => shortBreakInput.handleInputChange(e)}
         onBlur={shortBreakInput.handleInputBlur}
+        handleIncrease={shortBreakInput.handleIncrease}
+        handleDecrease={shortBreakInput.handleDecrease}
       />
-      <Input
-        className={styles.container}
-        type="number"
+
+      <TimerInput
         value={longBreakInput.value}
         htmlFor="long-break"
         id="long-break"
         labelText="long break"
         onChange={(e) => longBreakInput.handleInputChange(e)}
         onBlur={longBreakInput.handleInputBlur}
+        handleIncrease={longBreakInput.handleIncrease}
+        handleDecrease={longBreakInput.handleDecrease}
       />
     </div>
   );
