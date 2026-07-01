a4bd2ed2d90337866f5fa4ee107de31475019615
Anastasiia Ustin <2anastasiia@mail.ru>
2026-07-01 11:20:10 +0200

    feat: add disabled prop for increase and decrease value buttons

--- src/features/change-timer/ui/TimerInput/TimerInput.tsx
+++ src/features/change-timer/ui/TimerInput/TimerInput.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import IconChevronUp from "@/shared/assets/images/icon-chevron-up.svg";
 import IconChevronDown from "@/shared/assets/images/icon-chevron-down.svg";
 import styles from "./TimerInput.module.css";
+import { TIMER_MAX_VALUE, TIMER_MIN_VALUE } from "@/shared/constants";
 
 interface TimerInputProps extends Omit<InputProps, "className" | "type"> {
   handleIncrease: () => void;
@@ -15,14 +16,25 @@ const TimerInput: FC<TimerInputProps> = ({
   handleDecrease,
   ...inputProps
 }) => {
+  const isDisabledUp = Number(inputProps.value) >= TIMER_MAX_VALUE;
+  const isDisabledDown = Number(inputProps.value) <= TIMER_MIN_VALUE;
+
   return (
     <div className={styles.parent}>
       <Input type="number" className={styles.container} {...inputProps} />
       <div className={styles.buttons}>
-        <Button className={`${styles.btn}`} onClick={handleIncrease}>
+        <Button
+          disabled={isDisabledUp}
+          className={`${styles.btn}`}
+          onClick={handleIncrease}
+        >
           <IconChevronUp className={`${styles.arrow}`} />
         </Button>
-        <Button className={`${styles.btn}`} onClick={handleDecrease}>
+        <Button
+          disabled={isDisabledDown}
+          className={`${styles.btn}`}
+          onClick={handleDecrease}
+        >
           <IconChevronDown className={`${styles.arrow}`} />
         </Button>
       </div>
