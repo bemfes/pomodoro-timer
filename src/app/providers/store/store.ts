da724505511b4ffde365bbb9794519f763cd8c08
Anastasiia Ustin <2anastasiia@mail.ru>
2026-07-06 19:32:02 +0200

    feat: add whitelist to persistConfig

--- src/app/providers/store/store.ts
+++ src/app/providers/store/store.ts
@@ -16,6 +16,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["settingsReducer"],
 };
 
 const rootReducer = combineReducers({
