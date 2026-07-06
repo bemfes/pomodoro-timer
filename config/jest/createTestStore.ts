8ca836be6614c5fcc05abe0bc306bced1b93c55c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-07-06 11:39:15 +0200

    fix: add rootReducer

--- config/jest/createTestStore.ts
+++ config/jest/createTestStore.ts
@@ -1,12 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import timerSlice from "../../src/entities/timer";
 import settingsSlice from "../../src/entities/settings";
 
+const rootReducer = combineReducers({
+  settingsReducer: settingsSlice,
+  timerReducer: timerSlice,
+});
+
 export function createTestStore() {
   return configureStore({
-    reducer: {
-      timerReducer: timerSlice,
-      settingsReducer: settingsSlice,
-    },
+    reducer: rootReducer,
   });
 }
